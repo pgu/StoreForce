67fc19037ee72b63635142fae8d5a171485593a4
Guilcher Pascal <pgu.dev@gmail.com>
2011-07-26 23:29:43 +0200

    add pom files for gwt maven project. missing the gae setup

---
+++ web/src/main/java/com/storeForce/client/StoreForceEntryPoint.java
@@ -0,0 +1,13 @@
+package com.storeForce.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+public class StoreForceEntryPoint implements EntryPoint {
+
+    @Override
+    public void onModuleLoad() {
+        
+    }
+
+}
